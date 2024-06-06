9a286c75d0edc78ca52c49268a121d6fd2872cfa
gabrielpereiravaleverde <gabriel.pereira@vale-verde.com>
2024-06-06 09:42:22 -0300

    adição de imagens

--- 01_1.Integração_de_Dados.py
+++ 01_1.Integração_de_Dados.py
@@ -1,8 +1,28 @@
 import streamlit as st
 import pandas as pd 
 from db.connection import incrementar_acesso
+from PIL import Image
 
 st.set_page_config(page_title="Integração de Dados", layout="wide", page_icon=":newspaper:")
+
+col1, col2, col3, col4 = st.columns([1,1,1,1])
+
+with col1:
+    image = Image.open("assets/Imagem2.png")
+    new_image = image.resize((125, 50))
+    st.image(new_image)
+with col2:
+    image = Image.open("assets/Imagem1 1.jpg")
+    new_image = image.resize((150, 50))
+    st.image(new_image)
+with col3:
+    image = Image.open("assets/Imagem3.png")
+    new_image = image.resize((125, 50))
+    st.image(new_image)
+with col4:
+    image = Image.open("assets/Imagem4.png")
+    new_image = image.resize((125, 50))
+    st.image(new_image)
 
 st.title("Integração de Dados do MINE Risk")
 
